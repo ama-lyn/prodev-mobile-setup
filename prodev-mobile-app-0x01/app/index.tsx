9be9cc20578cfe8323a24315cf4e9db6ae499657
Gwendolyn <132227466+ama-lyn@users.noreply.github.com>
2025-07-28 09:51:43 +0300

    fix: checker :)

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -13,6 +13,7 @@ export default function Index() {
           React Native provides you a single codebase for cross platforms
         </Text>
         <Text style={styles.smallText}>ALX is awesome</Text>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>\r\n  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>\r\n  <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
   );
